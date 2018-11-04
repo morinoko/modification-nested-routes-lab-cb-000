a3a31835900aa7d2d312d4be347199ae642c5819
Felice Forby <feliceforby@gmail.com>
2018-11-04 23:45:58 +0000

    minor refactor

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,6 +2,7 @@ class SongsController < ApplicationController
   def index
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
+      
       if @artist.nil?
         redirect_to artists_path, alert: "Artist not found"
       else
@@ -16,7 +17,7 @@ class SongsController < ApplicationController
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
       @song = @artist.songs.find_by(id: params[:id])
-      
+
       if @song.nil?
         redirect_to artist_songs_path(@artist), alert: "Song not found"
       end
@@ -68,4 +69,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name, :artist_id)
   end
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :artists do
     resources :songs, only: [:index, :show, :create, :edit]
   end
+  
   resources :songs
 end
